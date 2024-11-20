74881e8f244c9d0a4409726c9242114ab330e94f
Meghana Maringanti <megha.edu20@gmail.com>
2024-11-19 21:41:52 -0700

    Adding raw dataset

---
+++ queries.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import re
+
+with open("data/queries.txt", 'r') as file:
+    lines = file.readlines()
+
+
+# Extract content inside double quotes
+def extract_query(line):
+    """
+    This is function is used to extract query from each line.
+    :param line: line from the text file
+    :return: content if pattern matches, None is not
+    """
+    match = re.search(r'"([^"]*)"', line)
+    if match:
+        return match.group(1)
+    return None
+
+
+data = [extract_query(line) for line in lines if extract_query(line)]
+
+# Convert to DataFrame
+df = pd.DataFrame(data, columns=["query"])
+file_name = "data/queries.csv"
+df.to_csv(file_name, index=False)
+
+print("Queries have been written to excel_file")
